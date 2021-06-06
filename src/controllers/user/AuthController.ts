b3fa905ae0e66da4a7984ce960c4a369446203c3
Fallenefc <arylmoraesn@gmail.com>
2021-06-06 16:11:30 -0600

    added login and me routes

--- src/controllers/user/AuthController.ts
+++ src/controllers/user/AuthController.ts
@@ -1,8 +1,8 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
-import { Response } from 'express';
-import { createQueryBuilder, getConnection } from 'typeorm';
+import { Response, Request } from 'express';
+import { getConnection } from 'typeorm';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
@@ -13,7 +13,7 @@ import { User } from '../../entities/User';
 dotenv.config();
 
 class AuthController {
-  async register(req: AuthRequest, res: Response): Promise<any> {
+  async register(req: Request, res: Response): Promise<any> {
     try {
       const {
         username, email, password, gender,
@@ -68,6 +68,63 @@ class AuthController {
           },
           token,
         });
+      });
+      return null;
+    } catch (error) {
+      return errorHandler(res, 500, 'Server error');
+    }
+  }
+
+  async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+      const user = await getConnection()
+        .createQueryBuilder()
+        .select('user')
+        .from(User, 'user')
+        .where('user.email = :email', { email })
+        .getOne();
+
+      if (!user) return errorHandler(res, 404, 'A user with this email does not exist');
+
+      const isValid = await bcrypt.compare(password, user.password);
+
+      if (!isValid) return errorHandler(res, 401, 'Invalid password');
+
+      const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+        expiresIn: 1000 * 60 * 60 * 24 * 7,
+      });
+
+      return res.status(200).json({
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+        token,
+      });
+    } catch (err) {
+      return errorHandler(res, 500, 'Server error');
+    }
+  }
+
+  async me(req: AuthRequest, res: Response) {
+    try {
+      const { user } = req;
+
+      const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+        expiresIn: 1000 * 60 * 60 * 24 * 7,
+      });
+
+      return res.status(200).json({
+        user: {
+          email: user.email,
+          username: user.username,
+          id: user.id,
+          gender: user.gender,
+          role: user.role,
+        },
+        token,
       });
     } catch (error) {
       return errorHandler(res, 500, 'Server error');
