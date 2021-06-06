68c7c8e0d68c99ff27f7a8eda380a757d83103c1
Fallenefc <arylmoraesn@gmail.com>
2021-06-06 16:13:42 -0600

    added login and me routes

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 import express from 'express';
+import authMiddleware from '../middleware/auth';
 import AuthController from '../controllers/user/AuthController';
 
 const UserAuthController = new AuthController();
@@ -11,8 +12,8 @@ router.get('/', () => console.log('fuck'));
 
 // Login and Signup
 router.post('/user/register', UserAuthController.register);
-// router.post('/login', ProviderControllers.login);
-// router.get('/me', authMiddleware, ProviderControllers.profile);
+router.post('/user/login', UserAuthController.login);
+router.get('/user/me', authMiddleware, UserAuthController.me);
 // router.post('/forgotPassword', ProviderControllers.forgotPassword);
 // router.post('/resetPassword', ProviderControllers.resetPassword);
 // router.get('/verify/:token', ProviderControllers.verify);
